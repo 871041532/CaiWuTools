5cdbd318f77d6b72b72b251078433db9c2aad8e9
VectorZhou <871041532@outlook.com>
2020-04-29 11:48:02 +0800

    吊东西

--- Globals.py
+++ Globals.py
@@ -19,166 +19,14 @@ class Globals_Class(object):
 		# 制单人
 		self.eas_name = ""
 		title_dict = {
-				"pangjm-yln": "金梅君御用",
-				"jiangys02": "珊珊专用",
-				"zhoukaibing": "金梅君御用",
-				"Administrator": "金梅君御用",
-		}
-		eas_name_dict = {
-			"pangjm-yln":"pangjinmei",
-			"jiangys02":"jiangys02",
-			"zhoukaibing":"zhoukaibing",
-			"Administrator":"Administrator",
+				"Administrator": "111",
+				"PC":"222",
 		}
 		if self.user not in title_dict:
 			self.user = None
 		else:
 			self.user_title = title_dict[self.user]
 			self.desktop_path = "C:/Users/"+ self.user +"/Desktop/"
-			self.eas_name = eas_name_dict[self.user]
-
-		# 凭证表头
-		self.pingzheng_title = [
-            "公司",
-            "记账日期",
-            "业务日期",
-            "会计期间",
-            "凭证类型",
-            "凭证号",
-            "分录号",
-            "摘要",
-            "科目",
-            "币种",
-            "汇率",
-            "方向",
-            "原币金额",
-            "数量",
-            "单价",
-            "借方金额",
-            "贷方金额",
-            "制单人",
-            "过账人",
-            "审核人",
-            "附件数量",
-            "过账标记",
-            "机制凭证模块",
-            "删除标记",
-            "凭证序号",
-            "单位",
-            "参考信息",
-            "是否有现金流量",
-            "现金流量标记",
-            "业务编号",
-            "结算方式",
-            "结算号",
-            "辅助账摘要",
-            "核算项目1",
-            "编码1",
-            "名称1",
-            "核算项目2",
-            "编码2",
-            "名称2",
-            "核算项目3",
-            "编码3",
-            "名称3",
-            "核算项目4",
-            "编码4",
-            "名称4",
-            "核算项目5",
-            "编码5",
-            "名称5",
-            "核算项目6",
-            "编码6",
-            "名称6",
-            "核算项目7",
-            "编码7",
-            "名称7",
-            "核算项目8",
-            "编码8",
-            "名称8",
-            "发票号",
-            "换票证号",
-            "客户",
-            "费用类别",
-            "收款人",
-            "物料",
-            "财务组织",
-            "供应商",
-            "辅助账业务日期",
-            "到期日",
-        ]
-		self.liuliang_title = ["公司", "记账日期", "会计期间", "凭证类型", "凭证号", "币种", "分录号", "对方分录号", "主表信息", "附表信息", "原币", "本位币", "报告币", "主表金额系数",
-                "附表金额系数", "性质", "核算项目1", "编码1", "名称1", "核算项目2", "编码2", "名称2", "核算项目3", "编码3", "名称3", "核算项目4", "编码4",
-                "名称4", "核算项目5", "编码5", "名称5", "核算项目6", "编码6", "名称6", "核算项目7", "编码7", "名称7", "核算项目8", "编码8", "名称8"]
-		self.pingzheng_demo = [
-			"E018",
-			"",
-			"",
-			"",
-			"",
-			"",
-			"",
-			"",
-			"",
-			"BB01",
-			1,
-			"",
-			"",
-			0,
-			0,
-			"",
-			"",
-			self.eas_name,
-			"",
-			"",
-			1,
-			"FALSE",
-			"",
-			"FALSE",
-			"1545383744616--0",
-			"",
-			"",
-			"",
-			"",
-			"",
-			"",
-			"",
-			"",
-			"",
-			"",
-			"",
-			"",
-			"",
-			"",
-			"",
-			"",
-			"",
-			"",
-			"",
-			"",
-			"",
-			"",
-			"",
-			"",
-			"",
-			"",
-			"",
-			"",
-			"",
-			"",
-			"",
-			"",
-			"",
-			"",
-			"",
-			"",
-			"",
-			"",
-			"",
-			"",
-			"",
-			"",
-		]
 
 	# 处理一下日期格式
 	def eval_date_format(self, file_path, col_names):
@@ -266,13 +114,5 @@ class Globals_Class(object):
 		year = date_str.split('/')[0]
 		day = date_str.split('/')[2]
 		return year, month, day, last_month
-
-	# 获取凭证头索引
-	def get_pingzheng_idx(self, title):
-		return self.pingzheng_title.index(title)
-
-	# 获取空的模板
-	def get_origin_excel_data(self):
-		return {"凭证":[self.pingzheng_title],"现金流量":[self.liuliang_title]}
 
 Globals = Globals_Class()
--- Index.py
+++ Index.py
@@ -3,15 +3,6 @@ from PyQt5.QtCore import *
 from PyQt5.QtGui import *
 import sys
 import getpass
-import YinHangDuiZhang
-import GongXiangDaoRu
-import JieZhuanYunPos
-import JieZhuanShuJuCaiJiSheBei
-import ZhiFuBaoPark
-import WeiXinPark
-import YueMoJieZhuanMain
-import YueChuQueRenMain
-import ChengBenFeiYongJiTi
 from Globals import Globals
 
 
@@ -24,34 +15,34 @@ class ShowWindow(QWidget):
 
 	def initUI(self):
 		mainLayout = QVBoxLayout()
-		self.button1 = QPushButton("银行对账")
-		self.button1.clicked.connect(self.click_select_my)
-		self.button2 = QPushButton("共享导入")
-		self.button2.clicked.connect(self.click_select_bank)
+		self.button1 = QPushButton("1")
+		self.button1.clicked.connect(self.select_one)
+		self.button2 = QPushButton("2")
+		self.button2.clicked.connect(self.select_one)
 		jiezhuan_layout = QHBoxLayout()
-		self.button3 = QPushButton("结转云Pos")
-		self.button3.clicked.connect(self.click_jiezhuanyun_pos)
-		self.button4 = QPushButton("结转数据采集设备")
-		self.button4.clicked.connect(self.JieZhuanShuJuCaiJiSheBei)
+		self.button3 = QPushButton("3")
+		self.button3.clicked.connect(self.select_one)
+		self.button4 = QPushButton("4")
+		self.button4.clicked.connect(self.select_one)
 		jiezhuan_layout.addWidget(self.button3)
 		jiezhuan_layout.addWidget(self.button4)
 
 		yuezhong_jiezhuan_layout = QHBoxLayout()
-		self.button7 = QPushButton("月末结转主合同")
-		self.button7.clicked.connect(self.YueZhongJieZhuanMain)
-		self.button8 = QPushButton("月初确认主合同")
-		self.button8.clicked.connect(self.YueChuQueRenMain)
+		self.button7 = QPushButton("5")
+		self.button7.clicked.connect(self.select_one)
+		self.button8 = QPushButton("6")
+		self.button8.clicked.connect(self.select_one)
 		yuezhong_jiezhuan_layout.addWidget(self.button7)
 		yuezhong_jiezhuan_layout.addWidget(self.button8)
 
-		self.button9 = QPushButton("成本费用计提")
-		self.button9.clicked.connect(self.ChengBenFeiYongJiTi)
+		self.button9 = QPushButton("7")
+		self.button9.clicked.connect(self.select_one)
 
 		zhifubao_layout = QHBoxLayout()
-		self.button5 = QPushButton("停车场支付宝返款")
-		self.button5.clicked.connect(self.ZhiFuBaoPark)
-		self.button6 = QPushButton("停车场微信返款")
-		self.button6.clicked.connect(self.WeiXinPark)
+		self.button5 = QPushButton("8")
+		self.button5.clicked.connect(self.select_one)
+		self.button6 = QPushButton("9")
+		self.button6.clicked.connect(self.select_one)
 		zhifubao_layout.addWidget(self.button5)
 		zhifubao_layout.addWidget(self.button6)
 
@@ -82,41 +73,9 @@ class ShowWindow(QWidget):
 		self.setWindowTitle(Globals.user_title)
 
 	# 选择了银行对账
-	def click_select_my(self):
-		Globals.current_win = YinHangDuiZhang.into()
-
-	# 选择了共享导入
-	def click_select_bank(self):
-		Globals.current_win = GongXiangDaoRu.into()
-
-	# 选择了结转云pos
-	def click_jiezhuanyun_pos(self):
-		Globals.current_win = JieZhuanYunPos.into()
-
-	# 选择了结转数据采集设备
-	def JieZhuanShuJuCaiJiSheBei(self):
-		Globals.current_win = JieZhuanShuJuCaiJiSheBei.into()
-
-	# 支付宝停车场
-	def ZhiFuBaoPark(self):
-		Globals.current_win = ZhiFuBaoPark.into()
-
-	# 微信停车场
-	def WeiXinPark(self):
-		Globals.current_win = WeiXinPark.into()
-
-	# 月末结转主合同
-	def YueZhongJieZhuanMain(self):
-		Globals.current_win = YueMoJieZhuanMain.into()
-
-	# 月初确认主合同
-	def YueChuQueRenMain(self):
-		Globals.current_win = YueChuQueRenMain.into()
-
-	# 成本费用计提
-	def ChengBenFeiYongJiTi(self):
-		Globals.current_win = ChengBenFeiYongJiTi.into()
-
+	def select_one(self):
+		pass
+		# Globals.current_win = YinHangDuiZhang.into()
 
 if __name__ == '__main__':
 	app = QApplication(sys.argv)
